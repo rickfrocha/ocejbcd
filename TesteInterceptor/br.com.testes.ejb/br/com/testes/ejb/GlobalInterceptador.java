5f8d7d19fe8b43c8ebb7f58f5e14fd539cebb97a
rickfrocha <rickfrocha@gmail.com>
2014-09-17 14:07:46 -0300

    Incluid interceptador para construtores. Note que para callbacks de construtores não existe retorno. Porem, mesmo assim, deve ser invocado explicitamente o metodo invocationContext.proceed(). Caso isto nao ocorra, o metodo de inicializado do EJB anotado com @PostConstruct não será invocado.

--- TesteInterceptor/br.com.testes.ejb/br/com/testes/ejb/GlobalInterceptador.java
+++ TesteInterceptor/br.com.testes.ejb/br/com/testes/ejb/GlobalInterceptador.java
@@ -1,9 +1,16 @@
 package br.com.testes.ejb;
 
+import javax.annotation.PostConstruct;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
 public class GlobalInterceptador {
+	
+	@PostConstruct
+	public void init(InvocationContext iv) throws Exception{
+		System.out.println("Global Interceptador >>> Inicializacao >>> Metodo: " + iv.getMethod());
+		iv.proceed();
+	}
 	
 	@AroundInvoke
 	public Object intercept(InvocationContext ic) throws Exception{
--- TesteInterceptor/br.com.testes.ejb/br/com/testes/ejb/VendaEJB.java
+++ TesteInterceptor/br.com.testes.ejb/br/com/testes/ejb/VendaEJB.java
@@ -1,12 +1,17 @@
 package br.com.testes.ejb;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.interceptor.ExcludeDefaultInterceptors;
 import javax.interceptor.Interceptors;
 
 @Stateless(name="vendaEJB")
-@ExcludeDefaultInterceptors
 public class VendaEJB implements Venda {
+	
+	@PostConstruct
+	public void init(){
+		System.out.println("Inicializando EJB Venda!!!");
+	}
 
 	@Interceptors(InterceptadorVenda.class)
 	public boolean addItem(Integer id) {
